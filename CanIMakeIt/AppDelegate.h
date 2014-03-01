47464d8e84bf5b3888faa31e425f4e8cba26578f
Dakshayani Padman <daksha.padman@gmail.com>
2014-03-01 17:03:15 -0500

    Initial Commit

---
+++ CanIMakeIt/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  CanIMakeIt
+//
+//  Created by YOGESH PADMAN on 3/1/14.
+//  Copyright (c) 2014 Dakshayani Padman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ CanIMakeItTests/CanIMakeItTests.m
@@ -0,0 +1,34 @@
+//
+//  CanIMakeItTests.m
+//  CanIMakeItTests
+//
+//  Created by YOGESH PADMAN on 3/1/14.
+//  Copyright (c) 2014 Dakshayani Padman. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CanIMakeItTests : XCTestCase
+
+@end
+
+@implementation CanIMakeItTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
