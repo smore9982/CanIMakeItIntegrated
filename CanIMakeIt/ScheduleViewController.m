2aec576cd6040eec97fadd38c113d1e570de5cef
yichungwang <yichungwang@hotmail.com>
2014-04-25 00:42:34 -0400

    s

--- CanIMakeIt/ScheduleViewController.m
+++ CanIMakeIt/ScheduleViewController.m
@@ -38,8 +38,6 @@
     
     self.dataHelper = [[DataHelper alloc] init];
     TripProfileModel* tripProfileModel =[self.dataHelper getDefaultProfileData];
-    StopModel* departureStation = [self.dataHelper getStopModelWithID:tripProfileModel.departureId];
-    StopModel* destinationStation = [self.dataHelper getStopModelWithID:tripProfileModel.destinationId];
     
     NSDateFormatter *outputFormatter = [[NSDateFormatter alloc] init];
     
@@ -50,8 +48,6 @@
     NSArray* tripTimes = [self.dataHelper getTripDepartureTimesForDepartureId:tripProfileModel.departureId DestinationID:tripProfileModel.destinationId onDate:date];
     [outputFormatter setDateFormat:@"MM-dd"];
     CurrentDate = [outputFormatter stringFromDate:today];
-    //NSLog(@"%@", CurrentDate);
-    //NSLog(@"%@", tripTimes);
     
     NSMutableString *a = [NSMutableString string];
     for (int i = 0; i < [tripTimes count]; i++){
@@ -69,10 +65,6 @@
     _TimeV.font = [UIFont fontWithName:@"AvenirNext-Heavy" size:15];
     _TimeV.textAlignment = NSTextAlignmentCenter;
     _TimeV.backgroundColor = [UIColor colorWithRed:0.6 green:0.8 blue:1 alpha:1];
-    
-    
-    
-
     
     // Do any additional setup after loading the view.
 }
