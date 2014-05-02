054a1cd335113f229b6c0029c624559d6ec0159c
yichungwang <yichungwang@hotmail.com>
2014-05-02 18:31:03 -0400

    Schedule View

--- CanIMakeIt/ScheduleViewController.m
+++ CanIMakeIt/ScheduleViewController.m
@@ -67,51 +67,114 @@
         }
     }
     
-    NSMutableString *a = [NSMutableString string];
-    for (int i = tripid; i < [tripTimes count]; i++){
-        NSString* nextTrain = [tripTimes objectAtIndex: i];
-        [outputFormatter setDateFormat:@"HH:mm:ss"];
-        NSDate *nexttrain = [outputFormatter dateFromString:nextTrain];
-        NSString *nexttrainhour = [outputFormatter stringFromDate:nexttrain];
-        [outputFormatter setDateFormat:@"mm:ss"];
-        NSString *nexttrainmin = [outputFormatter stringFromDate:nexttrain];
-        [outputFormatter setDateFormat:@"ss"];
-        NSString *nexttrainsec = [outputFormatter stringFromDate:nexttrain];
-        double nexttraintime = [nexttrainhour doubleValue] * 3600 + [nexttrainmin doubleValue] * 60 + [nexttrainsec doubleValue];
-        int suggesthour = nexttraintime / 3600;
-        int suggestmin = (nexttraintime - (suggesthour * 3600)) / 60;
-        NSString *AMPM;
-        if (suggesthour == 0) {
-            AMPM = @"AM";
-            suggesthour = 12;
-        }
-        else if (suggesthour < 12){
-            AMPM = @"AM";
-        }
-        else if (suggesthour == 12){
-            AMPM = @"PM";
-        }
-        else if (suggesthour < 24){
-            suggesthour = suggesthour - 12;
-            AMPM = @"PM";
-        }
-        NSString *TIME = [NSString stringWithFormat:@"%02d:%02d %@", suggesthour, suggestmin, AMPM];
-        [a appendString:TIME];
-        [a appendString:@"\n"];
+    NSMutableString *before = [NSMutableString string];
+    for (int i = 0; i < tripid; i++){
+        NSString *TIME = [self ChangeTimeFormatToAMPM:[tripTimes objectAtIndex:i]];
+        [before appendString:TIME];
+        [before appendString:@"\n"];
+    }
+    
+    NSString *now = [self ChangeTimeFormatToAMPM:[tripTimes objectAtIndex:tripid]];
+    
+    NSMutableString *after = [NSMutableString string];
+    for (int i = tripid + 1; i < [tripTimes count]; i++){
+        NSString *TIME = [self ChangeTimeFormatToAMPM:[tripTimes objectAtIndex:i]];
+        [after appendString:TIME];
+        [after appendString:@"\n"];
     }
     
     _Date.text = [NSString stringWithFormat:@"%@ Schedule", CurrentDate];
     _Date.textColor = [UIColor darkGrayColor];
     _Date.font = [UIFont fontWithName:@"AvenirNext-Heavy" size:25];
     _Date.textAlignment = NSTextAlignmentCenter;
-    
-    _TimeV.text = [NSString stringWithFormat:@"%@", a];
+
     _TimeV.textColor = [UIColor darkGrayColor];
     _TimeV.font = [UIFont fontWithName:@"AvenirNext-Heavy" size:15];
-    _TimeV.textAlignment = NSTextAlignmentCenter;
     _TimeV.backgroundColor = [UIColor colorWithRed:0.6 green:0.8 blue:1 alpha:1];
     
+    [self AttributedTextInUITextOnTimeBefore:before OnTimeNow:now OnTimeAfter:after];
+    _TimeV.textAlignment = NSTextAlignmentCenter;
+    [_TimeV scrollRangeToVisible:NSMakeRange([before length] + 72, 0)];
+    
     // Do any additional setup after loading the view.
+}
+
+-(NSString *)ChangeTimeFormatToAMPM:(NSString *) Time{
+    
+    NSString* nextTrain = Time;
+    NSDateFormatter *outputFormatter = [[NSDateFormatter alloc] init];
+    [outputFormatter setDateFormat:@"HH:mm:ss"];
+    NSDate *nexttrain = [outputFormatter dateFromString:nextTrain];
+    NSString *nexttrainhour = [outputFormatter stringFromDate:nexttrain];
+    [outputFormatter setDateFormat:@"mm:ss"];
+    NSString *nexttrainmin = [outputFormatter stringFromDate:nexttrain];
+    [outputFormatter setDateFormat:@"ss"];
+    NSString *nexttrainsec = [outputFormatter stringFromDate:nexttrain];
+    double nexttraintime = [nexttrainhour doubleValue] * 3600 + [nexttrainmin doubleValue] * 60 + [nexttrainsec doubleValue];
+    int suggesthour = nexttraintime / 3600;
+    int suggestmin = (nexttraintime - (suggesthour * 3600)) / 60;
+    NSString *AMPM;
+    if (suggesthour == 0) {
+        AMPM = @"AM";
+        suggesthour = 12;
+    }
+    else if (suggesthour < 12){
+        AMPM = @"AM";
+    }
+    else if (suggesthour == 12){
+        AMPM = @"PM";
+    }
+    else if (suggesthour < 24){
+        suggesthour = suggesthour - 12;
+        AMPM = @"PM";
+    }
+    NSString *TIME = [NSString stringWithFormat:@"%02d:%02d %@", suggesthour, suggestmin, AMPM];
+    return TIME;
+}
+
+-(void)AttributedTextInUITextOnTimeBefore:(NSString *)TimeBefore OnTimeNow:(NSString *)TimeNow OnTimeAfter:(NSString *)TimerAfter
+{
+    NSString *text = [NSString stringWithFormat:@"%@%@\n%@",
+                      TimeBefore,
+                      TimeNow,
+                      TimerAfter];
+    
+
+    // Define general attributes like color and fonts for the entire text
+    NSDictionary *attribs = @{
+                              NSForegroundColorAttributeName: _TimeV.textColor,
+                              NSFontAttributeName: _TimeV.font
+                              };
+    
+    NSMutableAttributedString *attributedText =
+    [[NSMutableAttributedString alloc] initWithString:text
+                                           attributes:attribs];
+    
+    // set font
+    UIFont *Font = [UIFont fontWithName:@"AvenirNext-Heavy" size:15];
+    
+    // before time attributes
+    UIColor *lightGrayColor = [UIColor lightGrayColor];
+    NSRange TimeBeforeRange = [text rangeOfString:TimeBefore];
+    [attributedText setAttributes:@{NSForegroundColorAttributeName:lightGrayColor,
+                                    NSFontAttributeName:Font}
+                            range:TimeBeforeRange];
+        
+    // now time attributes
+    UIColor *redColor = [UIColor redColor];
+    NSRange TimeNowRange = [text rangeOfString:TimeNow];
+    [attributedText setAttributes:@{NSForegroundColorAttributeName:redColor,
+                                    NSFontAttributeName:Font}
+                            range:TimeNowRange];
+        
+    // after time attributes
+    UIColor *darkGrayColor = [UIColor darkGrayColor];
+    NSRange TimeAfterRange = [text rangeOfString:TimerAfter];
+    [attributedText setAttributes:@{NSForegroundColorAttributeName:darkGrayColor,
+                                    NSFontAttributeName:Font}
+                            range:TimeAfterRange];
+    
+    _TimeV.attributedText = attributedText;
 }
 
 - (void)didReceiveMemoryWarning
